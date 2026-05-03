aee576602e6ae5f627b2dda68fe5a81908bf8daf
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2026-05-03 18:10:38 +0800

    Create tile.dart

---
+++ lib/service/tile.dart
@@ -0,0 +1,43 @@
+import 'package:quick_settings_with_flutter_plugins/quick_settings.dart';
+import 'package:mihomoR/service/control.dart';
+
+/// =======================
+/// Tile callbacks
+/// =======================
+
+@pragma('vm:entry-point')
+Tile onTileClicked(Tile tile) {
+  final isActive = tile.tileStatus == TileStatus.active;
+
+  if (isActive) {
+    stopMihomo();
+
+    tile
+      ..tileStatus = TileStatus.inactive
+      ..label = "mihomo"
+      ..drawableName = "quick_settings_base_icon"
+      ..contentDescription = "mihomo 已停止";
+  } else {
+    startMihomo();
+
+    tile
+      ..tileStatus = TileStatus.active
+      ..label = "mihomo"
+      ..drawableName = "quick_settings_base_icon"
+      ..contentDescription = "mihomo 已启动";
+  }
+
+  return tile;
+}
+
+@pragma('vm:entry-point')
+Tile? onTileAdded(Tile tile) {
+  tile.label = "mihomo";
+  tile.drawableName = "quick_settings_base_icon";
+  tile.contentDescription = "mihomo 核心控制";
+  tile.tileStatus = TileStatus.inactive;
+  return tile;
+}
+
+@pragma('vm:entry-point')
+void onTileRemoved() {}
