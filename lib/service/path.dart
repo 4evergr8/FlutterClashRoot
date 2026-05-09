a6b9b871825dacd88e22b2a58f028de7cddd4e89
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2026-05-09 13:30:55 +0800

    Update path.dart

--- lib/service/path.dart
+++ lib/service/path.dart
@@ -1,5 +1,4 @@
 final String subscriptionsPath = '/data/adb/mihomo/subscriptions.yaml';
 final String configPath = '/data/adb/mihomo/config.yaml';
 final String settingsPath = '/data/adb/mihomo/settings.yaml';
-final String mergePath = '/data/adb/mihomo/merge.yaml';
 final String overridePath = '/data/adb/mihomo/override.yaml';
