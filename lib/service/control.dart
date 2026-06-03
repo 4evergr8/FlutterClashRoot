d956652a948145f66288678c744c4ccf04adade5
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2026-06-03 22:34:15 +0800

    Update control.dart

--- lib/service/control.dart
+++ lib/service/control.dart
@@ -1,29 +1,29 @@
 import 'dart:io';
 
-import 'package:mihomoR/service/path.dart' show scriptPath;
-
+import 'package:mihomoR/service/path.dart';
 
 /// kill 分支
 Future<String> stopMihomo() async {
-  final result = await Process.run("sh", [scriptPath, "kill"]);
+  final result = await Process.run("su", ["-c", "sh $scriptPath kill"]);
   return result.stdout.toString().trim() + result.stderr.toString();
 }
 
 /// start 分支
 Future<String> startMihomo() async {
-  await Process.run("sh", [scriptPath, "kill"]);
-  final result = await Process.run("sh", [scriptPath, "start"]);
+  await Process.run("su", ["-c", "sh $scriptPath kill"]);
+
+  final result = await Process.run("su", ["-c", "sh $scriptPath start"]);
   return result.stdout.toString().trim() + result.stderr.toString();
 }
 
 /// test 分支
 Future<String> testMihomo() async {
-  final result = await Process.run("sh", [scriptPath, "test"]);
+  final result = await Process.run("su", ["-c", "sh $scriptPath test"]);
   return result.stdout.toString().trim() + result.stderr.toString();
 }
 
 /// check 分支
 Future<String> checkMihomo() async {
-  final result = await Process.run("sh", [scriptPath, "check"]);
+  final result = await Process.run("su", ["-c", "sh $scriptPath check"]);
   return result.stdout.toString().trim() + result.stderr.toString();
 }
