e4db8563e88be3bd9dc406b9fb697447a8a8ba95
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2026-06-05 21:11:04 +0800

    Update GeneratedPluginRegistrant.java

--- android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -46,6 +46,11 @@ public final class GeneratedPluginRegistrant {
       Log.e(TAG, "Error registering plugin quick_settings_with_flutter_plugins, io.apparence.quick_settings.QuickSettingsPlugin", e);
     }
     try {
+      flutterEngine.getPlugins().add(new com.mrrhak.screen_lock_detector.ScreenLockDetectorPlugin());
+    } catch (Exception e) {
+      Log.e(TAG, "Error registering plugin screen_lock_detector, com.mrrhak.screen_lock_detector.ScreenLockDetectorPlugin", e);
+    }
+    try {
       flutterEngine.getPlugins().add(new io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin());
     } catch (Exception e) {
       Log.e(TAG, "Error registering plugin shared_preferences_android, io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin", e);
