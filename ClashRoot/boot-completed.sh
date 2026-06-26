cd6d18becf4edc8058e34c6876d2f277b2c09332
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2026-06-26 12:45:08 +0800

    Update boot-completed.sh

--- ClashRoot/boot-completed.sh
+++ ClashRoot/boot-completed.sh
@@ -5,13 +5,14 @@ CLASH_BIN="./clash"
 
 # 命令字符串变量
 START_CMD="cd $CLASH_DIR && chmod +x $CLASH_BIN && nohup setsid $CLASH_BIN -d . >$CLASH_DIR/clash.log 2>&1 &"
-KILL_CMD="killall clash >/dev/null 2>&1"
+KILL_CMD="killall clash"
 TEST_CMD="cd $CLASH_DIR && chmod +x $CLASH_BIN && $CLASH_BIN -t -d ."
 CHECK_CMD="ps -p \$(pidof clash) -o pid,ppid,%cpu,%mem,cmd; cat /proc/\$(pidof clash)/status"
 
 CMD="$1"
 
 if [ "$CMD" = "start" ]; then
+    eval "$KILL_CMD"
     eval "$START_CMD"
     echo "启动完毕"
 elif [ "$CMD" = "kill" ]; then
