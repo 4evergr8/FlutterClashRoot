3279eed72f6792e2c73a21e08f0d8524d2698b0c
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2026-06-26 12:11:54 +0800

    Update control.dart

--- lib/service/control.dart
+++ lib/service/control.dart
@@ -7,14 +7,7 @@ Future<String> stopClash() async {
   return (result.stdout.toString() + result.stderr.toString()).trim();
 }
 
-Future<String> startClash() async {
-  await Process.run("su", ["-c", "sh", scriptPath, "kill"]);
-  final process = await Process.start("su", ["-c", "sh", scriptPath, "start"]);
-  process.stdout.drain();
-  process.stderr.drain();
-  await process.exitCode;
-  return "启动完毕";
-}
+
 
 Future<String> testClash() async {
   final result = await Process.run("su", ["-c", "sh", scriptPath, "test"]);
