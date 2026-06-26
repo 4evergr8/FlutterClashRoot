9038a3a13aacfb59e2b6a9f9166ebf5a4da3f9a7
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2026-06-26 12:45:11 +0800

    Update control.dart

--- lib/service/control.dart
+++ lib/service/control.dart
@@ -1,20 +1,40 @@
 import 'dart:io';
 
 import 'package:clashroot/service/path.dart';
+import 'package:quick_settings_with_flutter_plugins/quick_settings.dart';
 
-Future<String> stopClash() async {
+Future<String> clashKill() async {
+  await QuickSettings.syncTile(
+    Tile(
+      label: "ClashRoot",
+      tileStatus: TileStatus.inactive,
+      drawableName: 'alarm_off',
+      contentDescription: "Clash核心已停止",
+    ),
+  );
   final result = await Process.run("su", ["-c", "sh", scriptPath, "kill"]);
   return (result.stdout.toString() + result.stderr.toString()).trim();
 }
 
+Future<String> clashStart() async {
+  await QuickSettings.syncTile(
+    Tile(
+      label: "ClashRoot",
+      tileStatus: TileStatus.active,
+      drawableName: 'alarm_on',
+      contentDescription: "Clash核心已启动",
+    ),
+  );
+  final result = await Process.run("su", ["-c", "sh", scriptPath, "start"]);
+  return (result.stdout.toString() + result.stderr.toString()).trim();
+}
 
-
-Future<String> testClash() async {
+Future<String> clashTest() async {
   final result = await Process.run("su", ["-c", "sh", scriptPath, "test"]);
   return (result.stdout.toString() + result.stderr.toString()).trim();
 }
 
-Future<String> checkClash() async {
+Future<String> clashCheck() async {
   final result = await Process.run("su", ["-c", "sh", scriptPath, "check"]);
   return (result.stdout.toString() + result.stderr.toString()).trim();
 }
