925595396638419a290cf15a4648d4a9cbba9228
4evergr8 <137018415+4evergr8@users.noreply.github.com>
2026-06-29 12:01:59 +0800

    Update boot-completed.sh

--- ClashRoot/boot-completed.sh
+++ ClashRoot/boot-completed.sh
@@ -1,51 +1,77 @@
 #!/system/bin/sh
 
 CLASH_DIR="/data/adb/modules/ClashRoot"
-CLASH_BIN="./clash"
-
-START_CMD="cd $CLASH_DIR && setsid $CLASH_BIN -d . >$CLASH_DIR/clash.log 2>&1 &"
-KILL_CMD="killall clash"
-TEST_CMD="cd $CLASH_DIR && $CLASH_BIN -t -d ."
-CHECK_CMD="ps -p \$(pidof clash) -o pid,ppid,%cpu,%mem,cmd; cat /proc/\$(pidof clash)/status"
-
+CLASH_BIN="$CLASH_DIR/clash"
+CLASH_LOG="$CLASH_DIR/clash.log"
 YQ="$CLASH_DIR/yq"
 WGET="busybox wget"
-
 BASE="$CLASH_DIR/data.yaml"
 OVERRIDE="$CLASH_DIR/override.yaml"
 OUT_DIR="$CLASH_DIR/config"
 CONFIG="$CLASH_DIR/config.yaml"
-
 CMD="$1"
+start() {
+    setsid "$CLASH_BIN" -d "$CLASH_DIR" >"$CLASH_LOG" 2>&1 &
+}
+kill() {
+    killall clash
+}
+apply_config() {
+    $YQ eval-all '
+        select(fileIndex == 1) as $a
+        | select(fileIndex == 0) as $b
+        | $b * $a
+    ' "$OVERRIDE" "$1" > "$CONFIG"
+}
 
 if [ "$CMD" = "start" ]; then
-    eval "$KILL_CMD"
-    eval "$START_CMD"
+    kill
+    start
     echo "启动完毕"
 
 elif [ "$CMD" = "kill" ]; then
-    eval "$KILL_CMD"
+    kill
     echo "停止完毕"
 
 elif [ "$CMD" = "test" ]; then
-    eval "$TEST_CMD"
+    "$CLASH_BIN" -t -d "$CLASH_DIR"
 
 elif [ "$CMD" = "check" ]; then
-    eval "$CHECK_CMD"
+    ps -p \$(pidof clash) -o pid,ppid,%cpu,%mem,cmd; cat /proc/\$(pidof clash)/status
 
 elif [ "$CMD" = "yaml" ]; then
 
     BASE_YAML="$2"
+    apply_config "$BASE_YAML"
 
-    $YQ eval-all '
-        select(fileIndex == 0) as $a
-        | select(fileIndex == 1) as $b
-        | $b * $a
-    ' "$OVERRIDE" "$BASE_YAML" > "$CONFIG"
+elif [ "$CMD" = "loop" ]; then
+
+    ua=$($YQ eval -r '.ua' "$BASE")
+    id=$($YQ eval -r '.subscriptions[] | select(.select == true) | .id' "$BASE" | head -n 1)
+    link=$($YQ eval -r '.subscriptions[] | select(.select == true) | .link' "$BASE" | head -n 1)
+
+    $WGET --user-agent="$ua" -O "$OUT_DIR/$id.yaml" "$link"
+
+    apply_config "$OUT_DIR/$id.yaml"
+
+    ts=$(date +%s)
+
+    $YQ eval --arg ts "$ts" '
+        .subscriptions |= map(
+            if .select == true then
+                .update = ($ts | tonumber)
+            else
+                .
+            end
+        )
+    ' "$BASE" -i
+
+    kill
+    start
 
 else
-    eval "$KILL_CMD"
-    eval "$START_CMD"
+    kill
+    start
 
     while true; do
         sleep 3600
@@ -55,21 +81,14 @@ else
         if [ $((10#$HOUR % 8)) -eq 6 ]; then
 
             (
-                set +e
 
                 ua=$($YQ eval -r '.ua' "$BASE")
                 id=$($YQ eval -r '.subscriptions[] | select(.select == true) | .id' "$BASE" | head -n 1)
                 link=$($YQ eval -r '.subscriptions[] | select(.select == true) | .link' "$BASE" | head -n 1)
 
-
-
                 $WGET --user-agent="$ua" -O "$OUT_DIR/$id.yaml" "$link"
 
-                $YQ eval-all '
-                    select(fileIndex == 1) as $a
-                    | select(fileIndex == 0) as $b
-                    | $a + $b
-                ' "$OVERRIDE" "$OUT_DIR/$id.yaml" > "$CONFIG"
+                apply_config "$OUT_DIR/$id.yaml"
 
                 ts=$(date +%s)
 
@@ -83,12 +102,10 @@ else
                     )
                 ' "$BASE" -i
 
-
-                set -e
             ) || true
 
-            eval "$KILL_CMD"
-            eval "$START_CMD"
+            kill
+            start
         fi
 
     done
